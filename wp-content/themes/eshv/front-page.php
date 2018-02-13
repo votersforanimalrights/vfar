76f09e5e05d454fd30f95d21705463158cd13d56
Scott Taylor <scott.c.taylor@mac.com>
2018-02-12 23:15:28 -0500

    stuff

--- wp-content/themes/eshv/front-page.php
+++ wp-content/themes/eshv/front-page.php
@@ -10,5 +10,22 @@ get_header() ?>
   <section class="content">
     <?php the_content() ?>
   </section>
+  <section class="content animal-content">
+    <div class="animal-svg">
+      <?php get_template_part('svg/companion-animals'); ?>
+    </div>
+    <div class="animal-svg">
+      <?php get_template_part('svg/animals-used-for-food'); ?>
+    </div>
+    <div class="animal-svg">
+      <?php get_template_part('svg/animals-used-for-clothing'); ?>
+    </div>
+    <div class="animal-svg">
+      <?php get_template_part('svg/animals-in-the-wild'); ?>
+    </div>
+    <div class="animal-svg">
+      <?php get_template_part('svg/animals-used-for-entertainment'); ?>
+    </div>
+  </section>
 </article>
 <?php get_footer() ?>

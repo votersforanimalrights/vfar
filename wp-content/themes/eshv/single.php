7f0ca9e3d51427187a023ccf1e68367a0a17ec3f
Scott Taylor <scott.c.taylor@mac.com>
2019-01-21 17:05:31 -0500

    add author name and time, first pass

--- wp-content/themes/eshv/single.php
+++ wp-content/themes/eshv/single.php
@@ -1,7 +1,23 @@
 <?php
 the_post();
 
-get_header() ?>
+get_header();
+
+function vfar_time() {
+  $time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
+  if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
+    $time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
+  }
+
+  return sprintf(
+    $time_string,
+    get_the_date( DATE_W3C ),
+    get_the_date(),
+    get_the_modified_date( DATE_W3C ),
+    get_the_modified_date()
+  );
+}
+?>
 <article class="page-wrapper">
   <section class="content centered-content page-content">
     <div class="page-content-wrapper">
@@ -10,6 +26,15 @@ get_header() ?>
       </header>
       <?php the_post_thumbnail(); ?>
       <section class="post-content">
+        <p class="byline-time">
+          By <span class="byline">
+            <span class="author vcard"><?php echo get_the_author() ?></span>
+          </span>
+          <span class="posted-on">
+            <span class="screen-reader-text">Posted on</span>
+            <?php echo vfar_time() ?>
+          </span>
+        </p>
       <?php the_content() ?>
       </section>
     </div>

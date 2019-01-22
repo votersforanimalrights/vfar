e067274ca9d786b9a4dab44db1597441ff790569
Scott Taylor <scott.c.taylor@mac.com>
2019-01-21 19:08:32 -0500

    fix news archive styles

--- wp-content/themes/eshv/home.php
+++ wp-content/themes/eshv/home.php
@@ -7,7 +7,9 @@
       </header>
       <?php if ( have_posts() ): while ( have_posts() ): the_post(); ?>
         <article class="archive-post">
-          <?php the_post_thumbnail('medium'); ?>
+          <figure>
+            <?php the_post_thumbnail('medium-large'); ?>
+          </figure>
           <header>
             <h1><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h1>
           </header>

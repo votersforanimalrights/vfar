e8ffb3241e8df87671aee25be83785309ac86401
Scott Taylor <scott.c.taylor@mac.com>
2019-01-21 19:49:54 -0500

    add excerpt code

--- wp-content/themes/eshv/home.php
+++ wp-content/themes/eshv/home.php
@@ -10,9 +10,12 @@
           <figure>
             <?php the_post_thumbnail('medium-large'); ?>
           </figure>
-          <header>
-            <h1><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h1>
-          </header>
+          <section>
+            <header>
+              <h1><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h1>
+            </header>
+            <p><?php echo ! empty( $post->post_excerpt ) ? get_the_excerpt() : '' ?></p>
+          </section>
         </article>
       <?php endwhile; endif; ?>
     </div>

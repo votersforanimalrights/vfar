f7beac2773b1dfb3c128940d7c41b48bdce13853
Scott Taylor <scott.c.taylor@mac.com>
2020-05-31 20:21:26 -0400

    update title

--- wp-content/themes/eshv/header.php
+++ wp-content/themes/eshv/header.php
@@ -48,7 +48,7 @@ if ( is_singular() && ! is_front_page() ) {
 <meta property="og:url" content="<?php the_permalink() ?>" />
 <?php
   if ($is_election_center) {
-    $title = '2019 General Election Endorsements';
+    $title = '2020 New York Primary Election Endorsements - VOTE June 23!';
   } elseif ($is_bags) {
     $title = 'Voters for Animal Rights’ Policy Recommendation on Plastic Bag Reduction, Reuse, and Recycling';
     $description = 'New York should be enforcing the ban on plastic bags, not weakening it.';

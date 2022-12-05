dc42eba91c7fc08d6e451a4527819fe49c565d00
Scott Taylor <scott.c.taylor@mac.com>
2022-12-04 19:56:00 -0500

    remove space

--- wp-content/themes/eshv/templates/donate.php
+++ wp-content/themes/eshv/templates/donate.php
@@ -1,5 +1,4 @@
 <div id="paypal-button-container" style="margin: 20px 0"></div>
-<script src="https://www.paypal.com/sdk/js?client-id=Ad4J1IxmlNPyPYiAYKfDqqHZQWHtydT6IcKTWSm8zbltxeK3ZnJEEJB9-GfEcfZ0sxqQSl4uIcKnoHNj
-&enable-funding=venmo"></script>
+<script src="https://www.paypal.com/sdk/js?client-id=Ad4J1IxmlNPyPYiAYKfDqqHZQWHtydT6IcKTWSm8zbltxeK3ZnJEEJB9-GfEcfZ0sxqQSl4uIcKnoHNj&enable-funding=venmo"></script>
 <script>paypal.Buttons({style: {color: "gold", shape: "pill", layout: "horizontal"},}).render('#paypal-button-container')</script>
 <script src='https://actionnetwork.org/widgets/v3/fundraising/donate-to-empire-state-humane-voters?format=js&source=widget'></script><div id='can-fundraising-area-donate-to-empire-state-humane-voters' style='width: 100%'></div>
